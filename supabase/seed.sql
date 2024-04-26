3ec97172b9941425096d144b86493f7a6e3feb19
itsRoze <elewis9989@gmail.com>
2024-04-26 16:38:43 -0400

    new migration + seed

---
+++ supabase/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO questionnaire (question) VALUES
+('What is your name?'),
+('You won yourself a condo. In 5 words only, describe the aesthetic.'),
+('The end of the world was announced for tomorrow. What are you doing?');
