c148d7c1a13e528a19f81b8f855f4f7cd0b2aa61
itsRoze <elewis9989@gmail.com>
2024-05-03 21:26:49 -0400

    get user id

--- src/ui/qr-scanner.tsx
+++ src/ui/qr-scanner.tsx
@@ -1,7 +1,27 @@
 "use client";
 
 import { QrReader } from "react-qr-reader";
+import { useRouter } from "next/navigation";
 
 export const QrScanner = () => {
-  return <QrReader constraints={{ facingMode: "environment" }} />;
+  const router = useRouter();
+  return (
+    <QrReader
+      constraints={{ facingMode: "environment" }}
+      onResult={(result, error) => {
+        if (error) {
+          console.info(error);
+        } else if (result) {
+          const resultText = result.getText();
+          const parts = resultText.split("/");
+
+          // Extract the user ID (last part of the URL)
+          const userId = parts[parts.length - 1];
+          if (!userId) return;
+
+          router.push("/profile/" + userId);
+        }
+      }}
+    />
+  );
 };
