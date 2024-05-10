3b26caad181731ed34addd745146b23817927a40
itsRoze <elewis9989@gmail.com>
2024-05-10 16:50:16 -0400

    add match sub on scanner page

--- src/app/(main)/scan/page.tsx
+++ src/app/(main)/scan/page.tsx
@@ -1,11 +1,22 @@
-"use client";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 import { QrScanner } from "@/ui/qr-scanner";
+import { MatchSubscription } from "@/ui/match-subscription";
 
-export default function Page() {
+export default async function Page() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    redirect("/login");
+  }
+
   return (
     <>
       <section>
         <QrScanner />
+        <MatchSubscription currentUserId={user.id} />
       </section>
     </>
   );
