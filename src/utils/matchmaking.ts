1b1f7d1c6e8e47b597bc4376095b33e30960c907
itsRoze <elewis9989@gmail.com>
2024-05-16 13:42:57 -0400

    update questions and matchmaking

--- src/utils/matchmaking.ts
+++ src/utils/matchmaking.ts
@@ -1,7 +1,13 @@
 export const matchmakingLines = [
-  "Oof, one thing is for sure. Y’all should never be roommates",
   "Soulmates during the apocalypse",
-  "You may have just met your worst enemy",
+  "You may have just met your arch nemesis",
+  "You both cause the apocalypse together",
+  "You start a band together and go on a world tour",
+  "You end up sharing a cell in a Mexican prison",
+  "You form a cult together",
+  "You both end up on the FBI watchlist",
+  "For one to live the other must die",
+  "This person is your key to an early retirement",
 ];
 
 export const getRandomMatchmakingLine = () => {
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,4 +1,23 @@
-INSERT INTO questionnaire (question) VALUES
-('What is your name?'),
-('You won yourself a condo. In 5 words only, describe the aesthetic.'),
-('The end of the world was announced for tomorrow. What are you doing?');
+-- Delete existing data from the table
+DELETE FROM questionnaire;
+
+INSERT INTO
+  questionnaire (id, question)
+VALUES
+  (1, 'What is your name?'),
+  (
+    2,
+    'If you were a travel guide for an alien visiting Earth, what''s the first place you''d take them?'
+  ),
+  (
+    3,
+    'If you got arrested today what would it be for?'
+  ),
+  (
+    4,
+    'If an ex got to roast you but could only use 1 word, what would it be?'
+  ),
+  (
+    5,
+    'You pissed off an evil sorcerer and they are going to turn you into a vampire, werewolf, or a consultant but they’re into consent culture so they let you pick…'
+  );
