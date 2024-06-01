f7ac8d4814687ebe23d8d8c01bb98c95ce6b704e
itsRoze <elewis9989@gmail.com>
2024-05-31 20:57:26 -0400

    add subapbase auth

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -4,34 +4,34 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from '@remix-run/react';
+} from "@remix-run/react";
 
 import {
   json,
   type LinksFunction,
   type LoaderFunctionArgs,
-} from '@remix-run/node';
-import stylesheet from '~/tailwind.css?url';
-import { createClient } from './lib/supabase/server';
+} from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
+import { createClient } from "./lib/supabase/server";
 
 export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: stylesheet },
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const { supabase } = await createClient(request);
   const user = (await supabase.auth.getUser()).data.user;
   const isAuthenticated = !!user;
-  console.log('ISAUTH', isAuthenticated);
+  console.log("ISAUTH", isAuthenticated);
   return json({ isAuthenticated });
 };
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
